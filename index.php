21bce3072f7461908487b9629fd8f9ceed2f4d9f
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2022-10-18 22:42:04 +0200

    add basic HTML scheme

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Song Wishes For DJ</title>
+          <!-- Favicons -->
+          <link rel="shortcut icon" href="https://jorit.vasconezgerlach.de/assets/icons/favicons/favicon.ico" type="image/x-icon" sizes="96x96">
+          <link rel="icon" href="https://jorit.vasconezgerlach.de/assets/icons/favicons/favicon.png" type="image/png" sizes="144x144">
+          <link rel="apple-touch-icon" href="https://jorit.vasconezgerlach.de/assets/icons/favicons/apple-touch-icon.png" type="image/png" sizes="144x144">
+          <!-- Scripts -->
+          <link rel="stylesheet" href="page.css">
+</head>
+<body>
+
+          <main>
+                    <section id="search">
+                              <form action="javascript:">
+                                        <input type="text" placeholder="Song Name">
+                                        <input type="submit" value="Search">
+                              </form>
+                    </section>
+                    <section id="songList"></section>
+          </main>
+
+</body>
+</html>
