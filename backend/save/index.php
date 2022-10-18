549a88f30091518b36eb26fb041b25ae5a752d44
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2022-10-19 00:27:47 +0200

    add save track API

--- backend/save/index.php
+++ backend/save/index.php
@@ -4,6 +4,14 @@ include '../index.php';
 
 onPost(function() {
 
+          $data = getPostData();
+
+          $trackId = $data['trackId'];
+
+          putStorage($trackId);
+
+          $out['out'] = getStorage();
+
           return $out;
 
 });
